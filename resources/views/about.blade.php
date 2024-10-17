47349ae4dc5092f39c24a793380d9a8a55f7319e
Samuel Bosawer <zam.bosawer@gmail.com>
2024-10-17 20:07:30 +0900

    install Laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+</head>
+<body>
+<h1>About</h1>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis eaque libero qui doloribus quidem! Explicabo adipisci nam dolorem suscipit amet ut aut, deleniti maiores ea recusandae possimus iste omnis nemo enim perspiciatis maxime ipsum voluptates tempore est reiciendis sed quasi? Odit itaque maiores id voluptatum tempore, deserunt quibusdam fugit magnam!
+</p>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return 'Halaman Contact';
+});
+
+Route::get('/blog', function () {
+    return 'Halaman blog';
+});
